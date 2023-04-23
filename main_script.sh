6b6293be3444019036f79481bf54bb022e34c4e2
Siyan Luo <siyanluo@amazon.com>
2023-04-22 22:16:45 -0700

    Removed device flag;

--- main_script.sh
+++ main_script.sh
@@ -1,6 +1,6 @@
 for dataset in "enron_others" "diatoms_others" "imclef07a_others" "imclef07d_others" "cellcycle_FUN" "derisi_FUN" "eisen_FUN" "expr_FUN" "gasch1_FUN" "gasch2_FUN" "seq_FUN" "spo_FUN" "cellcycle_GO" "derisi_GO" "eisen_GO" "expr_GO" "gasch1_GO" "gasch2_GO" "seq_GO" "spo_GO"
 do
-    python main.py --dataset $dataset --seed 0 --device 3&
+    python main.py --dataset $dataset --seed 0
 done
 
 
